843fc4eddf80463dd934d91f3b66714181132cf0
NicolasCardoso93 <danielcardoso935@gmail.com>
2019-10-02 15:40:45 -0300

    1

---
+++ Bar/Bar/Barra.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bar
+{
+    public class Barra
+    {
+        public int capacidad { get; set; }
+        public List<Bebida> bebidas { get; set; }
+        public Queue<Cliente>clientes { get; set; }
+        public Barra()
+        {
+
+        }
+       
+
+        //public bool barraLlena(Cliente clientes)
+        //{
+
+        //}
+    }
+}
---
+++ Bar/Bar/Cliente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bar
+{
+    public class Cliente
+    {
+        public string nombre { get; set; }
+        public float plata { get; set; }
+        public float tiempoConsumo { get; set;}
+        public Queue<Bebida> bebidasDeseadas { get; set; }
+
+        public Cliente()
+        {
+
+        }
+        public void agregarBebidas(Bebida bebidas)
+        {
+            bebidasDeseadas.Enqueue(bebidas);
+        }
+
+        private bool teAlcanza(Bebida bebidas)
+        {
+            if (bebidas.precio < plata)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool stockDeBebidas(Bebida bebidas)
+        {
+            if (bebidas.stock == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void comprarBebida(Bebida bebidas)
+        {
+            stockDeBebidas(bebidas);
+            teAlcanza(bebidas);
+            verBebidas(bebidas);
+            bebidasDeseadas.Dequeue();
+            comprar(bebidas);
+        }
+        private void comprar(Bebida bebidas)
+        {
+            plata -= bebidas.precio; 
+        }
+        private void verBebidas(Bebida bebidas)
+        {
+            bebidasDeseadas.Peek();
+        }
+
+        
+            
+    }
+}
